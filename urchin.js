9844f38b50982e4a4d51639f72e944dbb0755b49
Yara Aguilar <yaraaguilar987@gmail.com>
2021-02-20 12:15:23 -0500

    added the math functions

---
+++ urchin.js
@@ -0,0 +1,31 @@
+// Program 1: Local
+
+const numeroUno = 21;
+const numeroDos = 7;
+
+function addYourNumbers (variableOne, variableTwo)
+{
+    return variableOne + variableTwo;
+}
+
+function subtractYourNumbers (variableOne, variableTwo)
+{
+    return variableOne - variableTwo;
+}
+
+function divideYourNumbers (variableOne, variableTwo)
+{
+    return variableOne / variableTwo;
+}
+
+function multiplyYourNumbers (variableOne, variableTwo)
+{
+    return variableOne * variableTwo;
+}
+
+addYourNumbers(numeroUno, numeroDos);
+subtractYourNumbers(numeroUno, numeroDos);
+divideYourNumbers(numeroUno, numeroDos);
+multiplyYourNumbers(numeroUno, numeroDos);
+
+// Program 2: Global
