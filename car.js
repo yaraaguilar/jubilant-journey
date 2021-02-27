ad2727f325b0e78c98e9d8f0fdc62cffb8ee1374
Yara Aguilar <yaraaguilar987@gmail.com>
2021-02-27 12:11:53 -0500

    made an in class assignment file

---
+++ car.js
@@ -0,0 +1,24 @@
+class car
+{
+    constructor(opts)
+    {
+        this.make = opts.make;
+        this.model = opts.model;
+        this.color = opts.color;
+        this.yearMade = opts.yearMade;
+        this.mpg = opts.mpg;
+        this.typeOfCar = opts.typeOfCar;
+        this.engine = opts.engine;
+        this.horsepower = opts.horsepower;
+    }
+    getMake()
+    {
+        return this.make;
+    }
+    getModel()
+    {
+        return this.make;
+    }
+
+    
+}
